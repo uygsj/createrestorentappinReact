f998421d03a5412cf64fe73d6b39f67dc9624118
uygsj <krutikhas@gmail.com>
2023-09-08 10:47:08 +0530

    Initial commit of src folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Fragment } from "react";
-import Header from "./components/Layout/Header";
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
-   <Fragment>
-     <Header/>
-     <main>
-
-      <Meals/>
-
-     </main>
-    
-   </Fragment>
-     
-   
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
